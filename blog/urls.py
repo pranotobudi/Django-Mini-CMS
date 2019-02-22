c7b36ea3cfe50172b44c595b91a7873f5bee694d
pranotobudi <pranotobudi.s@gmail.com>
2019-02-22 17:03:47 +0700

    add CRUD post functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, post_load
 urlpatterns = [
-    path('', views.Home, name='blog-home'),
+    # path('', views.Home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post-load/', post_load, name='post-load'),
+
     path('about/', views.About, name='blog-about'),
 ]
