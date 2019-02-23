1562492d9bfed5563c648deb821723ac40910601
pranotobudi <pranotobudi.s@gmail.com>
2019-02-23 08:31:12 +0700

    add password reset using gmail SMTP

--- django_mini_cms/settings/base.py
+++ django_mini_cms/settings/base.py
@@ -123,3 +123,10 @@ print('MEDIA_ROOT: ', MEDIA_ROOT)
 print('MEDIA_URL: ', MEDIA_URL)
 print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
 print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'djangominicms@gmail.com'
+EMAIL_HOST_PASSWORD = 'PasswordDjangominicms123'
