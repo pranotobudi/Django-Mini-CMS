264110b9ef10d950478486ac53012f5f0f6b09bf
pranotobudi <pranotobudi.s@gmail.com>
2019-02-23 11:38:28 +0700

    install whitenoise and add to settings.py

--- django_mini_cms/settings/base.py
+++ django_mini_cms/settings/base.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_mini_cms.urls'
@@ -107,6 +108,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
