7708552bd661a3ce0471e0e0b86b88ef43853ec7
pranotobudi <pranotobudi.s@gmail.com>
2019-02-24 00:55:45 +0700

    add boto3 and django-storage library, and add related settings.py for AWS

--- django_mini_cms/settings/base.py
+++ django_mini_cms/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku  
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'crispy_forms', 
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -140,4 +141,3 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'djangominicms@gmail.com'
 EMAIL_HOST_PASSWORD = 'PasswordDjangominicms123'
 
-django_heroku.settings(locals())
--- django_mini_cms/settings/production.py
+++ django_mini_cms/settings/production.py
@@ -22,4 +22,10 @@ DATABASES = {
     }
 }
 
+AWS_ACCESS_KEY_ID =  "AKIAIGUNMG5H4WWNAIAA"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = 'django-mini-cms'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 django_heroku.settings(locals())
