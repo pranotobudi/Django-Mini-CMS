9f2485e0385ac2b91a004a596a977be213f55d01
pranotobudi <pranotobudi.s@gmail.com>
2019-02-24 11:41:14 +0700

    enable django_heroku.settings(locals())

--- django_mini_cms/settings/production.py
+++ django_mini_cms/settings/production.py
@@ -31,4 +31,4 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
